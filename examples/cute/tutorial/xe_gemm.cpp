0a15adbdc007b54925e723aa83e7d32b70351b4b
Jacky, Deng <jacky.deng@intel.com>
2025-11-03 15:22:07 +0800

    Fixed build issues after merged from public repo. (#93)

--- examples/cute/tutorial/xe_gemm.cpp
+++ examples/cute/tutorial/xe_gemm.cpp
@@ -411,9 +411,8 @@ int main(int argc, char** argv)
 #endif
 
   test_case<uint4_t, uint4_t, uint32_t, 'R', 'C'>(Q, m, n, k, iterations, verify);
-  test_case<uint4_t, uint4_t, uint32_t, 'R', 'C'>(Q, m, n, k);
-  test_case<uint4_t, uint4_t, uint32_t, 'R', 'R'>(Q, m, n, k);
->>>>>>> public_sycl/main
+  test_case<uint4_t, uint4_t, uint32_t, 'R', 'C'>(Q, m, n, k, iterations, verify);
+  test_case<uint4_t, uint4_t, uint32_t, 'R', 'R'>(Q, m, n, k, iterations, verify);
 
   // Upconversion cases
   test_case<half_t, float_e5m2_t, float, 'R', 'R'>(Q, m, n, k, iterations, verify);
