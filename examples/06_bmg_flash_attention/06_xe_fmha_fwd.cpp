9cbb9f1229b8e2a2682858d2c031bdedade00583
Chen Xi <xi2.chen@intel.com>
2026-03-18 11:19:30 +0800

    Add tuned tileshape for CRI (#350)
    Signed-off-by: Chen, Xi2 <xi2.chen@intel.com>
    Co-authored-by: Jacky, Deng <jacky.deng@intel.com>

--- examples/06_bmg_flash_attention/06_xe_fmha_fwd.cpp
+++ examples/06_bmg_flash_attention/06_xe_fmha_fwd.cpp
@@ -111,11 +111,17 @@ int main(int argc, const char **argv) {
   using SubgroupLayoutQK = Layout<Shape<_8, _1, _1>>;
 
 #elif HEAD_DIM == 128
+#if !(defined(SYCL_INTEL_TARGET) && (SYCL_INTEL_TARGET == 35))
   using ShapeQK = Shape<_256, _32, _32>;
   using ShapePV = Shape<_256, _32, _32>;
   using ShapeOut = Shape<_256, _128>;
   using SubgroupLayoutQK = Layout<Shape<_16, _1, _1>>;
-
+#else
+  using ShapeQK = Shape<_256, _64, _64>;
+  using ShapePV = Shape<_256, _64, _64>;
+  using ShapeOut = Shape<_256, _128>;
+  using SubgroupLayoutQK = Layout<Shape<_16, _1, _1>>;
+#endif
 #elif HEAD_DIM == 192
   using ShapeQK = Shape<_256, _64, _32>;
   using ShapePV = Shape<_256, _32, _64>;
