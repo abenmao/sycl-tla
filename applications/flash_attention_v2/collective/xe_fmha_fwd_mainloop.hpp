f30ddf8029218d35c0b8daeabc1f0e8f16d4a12d
ratnampa <ratnam.parikh@intel.com>
2026-03-24 17:39:23 +0000

    On branch ratnampa/new_merge_v0.1.0_next_to_master_next Conflicts: 	CHANGELOG-SYCL.md 	README.md 	examples/cute/tutorial/CMakeLists.txt 	test/unit/common/filter_architecture.cpp

--- applications/flash_attention_v2/collective/xe_fmha_fwd_mainloop.hpp
+++ applications/flash_attention_v2/collective/xe_fmha_fwd_mainloop.hpp
@@ -591,15 +591,21 @@ struct FMHAFwdMainloop<XeDefault<Stages>, CausalMask_, UseScale_, F8kvF16mma_,
         }
       }
 
-      /* Apply softmax and scaling */
-      softmax(K == blk_k0, tSrS, tA_max, tA_sum, tArA);
+      /* Apply softmax and scaling (tA rescaling fused into GEMM2 VTile loop) */
+      auto rescale = softmax(K == blk_k0, tSrS, tA_max, tA_sum);
       reorder(tSrS, tArP);
 
-      /* GEMM 2: A += P * V, split in v dimension */
+      /* GEMM 2: A += P * V, split in v dimension.
+        tArA rescaling is fused to per-VTile */
       CUTLASS_PRAGMA_UNROLL
       for (int VV = 0; VV < VTiles; VV++) {
         copy(copy_v_cur, tVgV_cur(_,_,_,VV,k_idx), tVrV);
         reorder(tVrV, tArV);
+        if (K != blk_k0) {
+          CUTLASS_PRAGMA_UNROLL
+          for (int i = 0; i < tArA.size() / VTiles; i++)
+            tArA(_,_,_,VV)(i) *= broadcast<0>(rescale, tArA, i);
+        }
         if constexpr (UseScale && !FP4Input) {
           const int v_coord = get<1>(blk_qv) * VTiles * BLK_V + VV * BLK_V + (subgroup_id % ATOM_V) * SG_V;
           auto& tiled_copy_scaleP = get<0>(get<0>(scale_context_pv));
@@ -690,13 +696,11 @@ struct FMHAFwdMainloop<XeDefault<Stages>, CausalMask_, UseScale_, F8kvF16mma_,
 
   // Single step of blocked softmax.
   CUTLASS_DEVICE
-  void
+  FragSRow
   softmax(bool       first_block, // First softmax block?
           FragS    & tS,          // Softmax src/dst block
           FragSRow & tS_max,      // Softmax row-wise max accumulator
-          FragSRow & tS_sum,      // Softmax row-wise sum accumulator
-          FragA    & tA) {        // O accumulator (for rescaling)
-
+          FragSRow & tS_sum) {    // Softmax row-wise sum accumulator
     /* Compute row-wise maxima for this block */
     auto tS_bmax = reduce<1>(tS, sycl::maximum{});
 
@@ -713,16 +717,12 @@ struct FMHAFwdMainloop<XeDefault<Stages>, CausalMask_, UseScale_, F8kvF16mma_,
     for (int i = 0; i < tS.size(); i++)
       tS(i) = sycl::native::exp2(params.scale * tS(i) - broadcast<0>(tS_max, tS, i));
 
-    /* Rescale existing S sums and O accumulator */
+    /* Rescale existing S sums */
     if (!first_block) {
       CUTLASS_PRAGMA_UNROLL
       for (int i = 0; i < tS_sum.size(); i++) {
         tS_sum(i) *= rescale(i);
       }
-
-      CUTLASS_PRAGMA_UNROLL
-      for (int i = 0; i < tA.size(); i++)
-        tA(i) *= broadcast<0>(rescale, tA, i);
     }
 
     /* Update sums */
@@ -730,6 +730,8 @@ struct FMHAFwdMainloop<XeDefault<Stages>, CausalMask_, UseScale_, F8kvF16mma_,
     CUTLASS_PRAGMA_UNROLL
     for (int i = 0; i < tS_sum.size(); i++)
       tS_sum(i) += tS_bsum(i);
+
+    return rescale;
   }
 };
 
--- test/unit/common/filter_architecture.cpp
+++ test/unit/common/filter_architecture.cpp
@@ -116,6 +116,8 @@ void FilterArchitecture() {
    ,{architecture::intel_gpu_jgs, 2}
 #endif
     // TODO: Update this code section once we can retrieve arch info from oneAPI APIs.
+    {architecture::intel_gpu_bmg_g21, 1},
+    {architecture::intel_gpu_bmg_g31, 1}
   };
   // TODO: This is a temporary solution to support CRI. 
   // Once we can retrieve arch info from oneAPI APIs, we can remove this.
