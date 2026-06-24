160fb4b80fa2ee86ed3e0ccb1691cc881f5389e3
Jiexin-Zheng <jiexin.zheng@intel.com>
2026-06-24 16:26:47 +0800

    examples: FA: change tile shape for mxfp8 (#698)
    Co-authored-by: Jacky, Deng <jacky.deng@intel.com>

--- examples/06_bmg_flash_attention/06_xe_fmha_fwd_mxfp.cpp
+++ examples/06_bmg_flash_attention/06_xe_fmha_fwd_mxfp.cpp
@@ -116,11 +116,24 @@ int main(int argc, const char **argv) {
   using SubgroupLayoutQK = Layout<Shape<_8, _1, _1>>;
 
 #elif HEAD_DIM == 128
-  using ShapeQK = Shape<_128, _64, _32>;
-  using ShapePV = Shape<_128, _32, _64>;
-  using ShapeOut = Shape<_128, _128>;
-  using SubgroupLayoutQK = Layout<Shape<_16, _1, _1>>;
-
+  #if !(defined(SYCL_INTEL_TARGET) && (SYCL_INTEL_TARGET == 35))
+    using ShapeQK = Shape<_128, _64, _32>;
+    using ShapePV = Shape<_128, _32, _64>;
+    using ShapeOut = Shape<_128, _128>;
+    using SubgroupLayoutQK = Layout<Shape<_16, _1, _1>>;
+  #else
+    #if defined(IS_MX_FLOAT_E5M2) || defined(IS_MX_FLOAT_E4M3)
+      using ShapeQK = Shape<_512, _64, _64>;
+      using ShapePV = Shape<_512, _64, _64>;
+      using ShapeOut = Shape<_512, _128>;
+      using SubgroupLayoutQK = Layout<Shape<_32, _1, _1>>;
+    #else
+      using ShapeQK = Shape<_128, _64, _32>;
+      using ShapePV = Shape<_128, _32, _64>;
+      using ShapeOut = Shape<_128, _128>;
+      using SubgroupLayoutQK = Layout<Shape<_16, _1, _1>>;
+    #endif
+  #endif
 #elif HEAD_DIM == 192
   using ShapeQK = Shape<_256, _64, _32>;
   using ShapePV = Shape<_256, _32, _64>;
